4432bdf352640c0b8fb3467537eb0d9cbb7dbba3
JozoPejic <147092358+JozoPejic@users.noreply.github.com>
2023-10-12 20:39:24 +0200

    Add files via upload

---
+++ Vjezba2/jozo_pejic_zad1.cpp
@@ -0,0 +1,38 @@
+﻿//Napisati funkciju koja računa najveći i najmanji broj u nizu od n prirodnih
+//brojeva.Funkcija vraća tražene brojeve pomoću referenci.
+
+#include<iostream>
+
+void MinMax(int &min, int &max, int *a, int n)
+{
+	min = a[0];
+	max = a[0];
+
+	for (int i = 0; i < n; i++) {
+		if (a[i] > max){
+			max = a[i];
+		}
+		if (a[i] < min) {
+			min = a[i];
+		}
+	}
+}
+
+int main() {
+
+	int n, min, max;
+
+	std::cout << "Unesi broj clanova niza: ";
+	std::cin >> n;
+
+	int *a = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		std::cin >> a[i];
+	}
+
+	MinMax(min, max, a, n);
+
+	std::cout << "Najmanji clan niza je: " << min << std::endl;
+	std::cout << "Najveci clan niza je: " << max << std::endl;
+}
---
+++ Vjezba2/jozo_pejic_zad2.cpp
@@ -0,0 +1,33 @@
+﻿/*Napisati funkciju koja vraća referencu na neki element niza. Koristeći po-
+vratnu vrijednost funkcije kao lvalue uvećajte i-ti element niza za jedan.*/
+
+#include<iostream>
+
+int& lvalue(int *a) {
+
+	int& neki_element = a[2];
+
+	return neki_element;
+}
+
+int main() {
+
+	int n;
+	int neki_element;
+
+	std::cout << "Unesite broj clanova niza: " << std::endl;
+	std::cin >> n;
+
+	int *a = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		std::cin >> a[i];
+	}
+
+	lvalue(a)++;
+
+
+	for (int i = 0; i < n; i++) {
+		std::cout << a[i] << std::endl;
+	}
+}
